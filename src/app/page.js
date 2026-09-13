d8c754d6bc8dda0158874811beaec1f0a1d4a602
Umar Z <umarzzaib2@gmail.com>
2026-09-14 00:27:54 +0500

    Add Hero Component

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Landing_Navbar from "../../components/Landing_Navbar";
+import Right_Hero from "../../components/Right_Hero.jsx";
 
 export default function Home() {
   return (
     <>
     <Landing_Navbar></Landing_Navbar>
+    <Right_Hero></Right_Hero>
     </>
   );
 }
